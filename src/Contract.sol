a4c65fa20cb9789d6ae3c979ce139fa2d33b16c0
Kalai Mohan <mohan@Kalais-MacBook-Air.local>
2023-11-06 22:01:59 +0530

    first commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {
-    constructor() {}
+contract StatusUpdate {
+    uint256 constant MAX_CHARACTER_AMOUNT = 140;
+
+    mapping(address => string) public statuses;
+
+    event StatusUpdated(
+        address indexed user,
+        string newStatus,
+        uint256 timestamp
+    );
+
+    function setStatus(string memory _status) public {
+        require(
+            bytes(_status).length <= MAX_CHARACTER_AMOUNT,
+            "Status is too long"
+        );
+        statuses[msg.sender] = _status;
+        emit StatusUpdated(msg.sender, _status, block.timestamp);
+    }
+
+    function getStatus(address _user) public view returns (string memory) {
+        string memory status = statuses[_user];
+        if (bytes(status).length == 0) {
+            return "No status set";
+        } else {
+            return status;
+        }
+    }
 }
